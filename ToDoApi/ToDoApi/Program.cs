1c6cce328f7b810043ab74a67466f31ad806e06c
Biniam <Biniamt23@yahoo.com>
2020-02-09 16:10:07 -0800

    working with api

--- ToDoApi/ToDoApi/Program.cs
+++ ToDoApi/ToDoApi/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace ToDoApi
 {
     class Program
     {
-        static void Main(string[] args)
+        //create a new httpClient which is going to be used to get the response from our api. It will respond with all
+        //available users
+
+        HttpClient client = new HttpClient();
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program program = new Program();
+            await program.GetAllUsers();
+        }
+
+        public async Task GetAllUsers()
+        {
+            string response = await client.GetStringAsync("https://jsonplaceholder.typicode.com/users");
+            Console.WriteLine(response);
         }
     }
 }
